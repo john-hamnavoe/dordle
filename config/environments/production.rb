0d58240cd4df1da34dc798a04cdc354c0f55fef9
John H Wallace <john.wallace@hamnavoepartners.com>
2022-01-10 15:24:09 +0000

    seperate redis for session store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,9 +118,16 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.cache_store = :redis_cache_store, {driver: :hiredis, url: ENV.fetch("REDIS_URL")}
-  config.session_store :cache_store,
-    key: "_session",
-    compress: true,
-    pool_size: 5,
-    expire_after: 1.year  
+
+  config.session_store :redis_session_store, {
+    key: "_session_production",
+    serializer: :json,
+    redis: {
+      driver: :hiredis,
+      expire_after: 1.year,
+      ttl: 1.year,
+      key_prefix: "app:session:",
+      url: ENV.fetch("HEROKU_REDIS_GOLD_URL")
+    }
+  }
 end
